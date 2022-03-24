f7464fb6fa629a960c9d13ddc4a2488ca8b20514
DougMellon <dm@dougmellon.com>
2022-03-24 09:19:53 -0600

    Added deque

--- src/com/unco/Main.java
+++ src/com/unco/Main.java
@@ -1,8 +1,13 @@
 package com.unco;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Main {
 
+    Deque<Person> deque = new ArrayDeque<>();
+
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
